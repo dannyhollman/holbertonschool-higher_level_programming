a141979967b5bfdac1344032fecc9f1a7b0bdb69
Danny Hollman <dannyhollman@gmail.com>
2019-09-12 18:00:30 +0000

    [LOCAL][UPDATE] Fix output

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,22 +7,22 @@ def main():
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] not in "+_*/":
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
 
     if argv[2] == '+':
         print("{} + {} = {}".format(argv[1], argv[3],
               (add(int(argv[1]), int(argv[3])))))
-    if argv[2] == '-':
-        print("{} + {} = {}".format(argv[1], argv[3],
+    elif argv[2] == '-':
+        print("{} - {} = {}".format(argv[1], argv[3],
               (sub(int(argv[1]), int(argv[3])))))
-    if argv[2] == '*':
-        print("{} + {} = {}".format(argv[1], argv[3],
+    elif argv[2] == '*':
+        print("{} * {} = {}".format(argv[1], argv[3],
               (mul(int(argv[1]), int(argv[3])))))
-    if argv[2] == '/':
-        print("{} + {} = {}".format(argv[1], argv[3],
+    elif argv[2] == '/':
+        print("{} / {} = {}".format(argv[1], argv[3],
               (div(int(argv[1]), int(argv[3])))))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
 if __name__ == "__main__":
     main()
