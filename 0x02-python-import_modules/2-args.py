3fb5947c7c1ea187773e281c196cf524cbd51df7
Danny Hollman <dannyhollman@gmail.com>
2019-09-12 14:29:43 +0000

    [LOCAL][UPDATE] Fix execute when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,13 @@
 import sys
 
 
-if len(sys.argv) == 1:
-    print("0 arguments.")
-    exit()
-print("{:d} arguments:".format(len(sys.argv) - 1))
-for i in range(1, len(sys.argv)):
-    print("{:d}: {}".format(i, sys.argv[i]))
+def main():
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+        exit()
+    print("{:d} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{:d}: {}".format(i, sys.argv[i]))
+
+if __name__ == "__main__":
+    main()
