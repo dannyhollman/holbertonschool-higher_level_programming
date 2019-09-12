65396e2782fdaf92ce9962f0df471f54519f4172
Danny Hollman <dannyhollman@gmail.com>
2019-09-12 16:01:55 +0000

    [LOCAL][UPDATE] Fix output for one input

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,10 @@ def main():
     if len(sys.argv) == 1:
         print("0 arguments.")
         exit()
-    print("{:d} arguments:".format(len(sys.argv) - 1))
+    if len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{:d}: {}".format(i, sys.argv[i]))
 
