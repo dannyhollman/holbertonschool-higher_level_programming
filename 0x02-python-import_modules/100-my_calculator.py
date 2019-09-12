2dd6a0f01127e6774278992286cfe0c3f3137859
Danny Hollman <dannyhollman@gmail.com>
2019-09-12 17:52:03 +0000

    [LOCAL][UPDATE] Add all operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-import calculator_1
-import sys
+from calculator_1 import add, sub, mul, div
+from sys import argv
 
 
 def main():
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if sys.argv[2] not in "+_*/":
+    if argv[2] not in "+_*/":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    if sys.argv[2] == '+':
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3],
-              (int(sys.argv[1]) + int(sys.argv[3]))))
+
+    if argv[2] == '+':
+        print("{} + {} = {}".format(argv[1], argv[3],
+              (add(int(argv[1]), int(argv[3])))))
+    if argv[2] == '-':
+        print("{} + {} = {}".format(argv[1], argv[3],
+              (sub(int(argv[1]), int(argv[3])))))
+    if argv[2] == '*':
+        print("{} + {} = {}".format(argv[1], argv[3],
+              (mul(int(argv[1]), int(argv[3])))))
+    if argv[2] == '/':
+        print("{} + {} = {}".format(argv[1], argv[3],
+              (div(int(argv[1]), int(argv[3])))))
 
 if __name__ == "__main__":
     main()
