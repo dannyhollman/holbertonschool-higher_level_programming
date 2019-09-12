0f1682a51a1925d19da27b237f28f8a7f5768375
Danny Hollman <dannyhollman@gmail.com>
2019-09-12 14:19:38 +0000

    [LOCAL][INIT] Add file 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+
+if len(sys.argv) == 1:
+    print("0 arguments.")
+    exit()
+print("{:d} arguments:".format(len(sys.argv) - 1))
+for i in range(1, len(sys.argv)):
+    print("{:d}: {}".format(i, sys.argv[i]))
