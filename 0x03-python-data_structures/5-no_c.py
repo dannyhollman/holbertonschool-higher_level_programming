f577166db7f29c9db8e35d7612c93e8eadff62a6
Danny Hollman <dannyhollman@gmail.com>
2019-09-17 18:09:46 +0000

    [LOCAL][UPDATE] Remove .replace function

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.replace('c', '')
-    my_string = my_string.replace('C', '')
-    return my_string
+    my_string = list(my_string)
+    if 'c' in my_string:
+        my_string.remove('c')
+    if 'C' in my_string:
+        my_string.remove('C')
+    return ''.join(my_string)
