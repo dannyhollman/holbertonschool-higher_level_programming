51b241b20390fd7f4d17529f0516517cbee5b3c3
Danny Hollman <dannyhollman@gmail.com>
2019-09-17 17:25:07 +0000

    [GLOBAL][UPDATE] Remove files from wrong directory

--- 2-replace_in_list.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    my_list[idx] = element
-    return (my_list)
--- 7-add_tuple.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    if not
-    x = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
-    return x
