589f20e310e9c4bbf68188fa25316fda3cafe637
Danny Hollman <dannyhollman@gmail.com>
2019-09-17 21:07:27 +0000

    [LOCAL][UPDATE] Fix output fot all negative input

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    i = 0
     if not my_list:
         return None
+    i = my_list[0]
     for x in my_list:
         if x > i:
             i = x
