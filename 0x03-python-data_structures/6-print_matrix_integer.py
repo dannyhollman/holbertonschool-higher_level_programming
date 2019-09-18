7151fb9cdf44d9e3ae4aeb8970e6eeea0ed07d82
Danny Hollman <dannyhollman@gmail.com>
2019-09-18 00:17:22 +0000

    [LOCAL][UPDATE] Fix output for uneven matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(mtrx=[[]]):
-    column = len(mtrx)
-    row = len(mtrx[0])
-    for x in range(column):
-        for y in range(row):
-            print("{:d}".format(mtrx[x][y]), end=" " if y < column - 1 else "")
+    for x in range(len(mtrx)):
+        for y in range(len(mtrx[x])):
+            print("{:d}".format(mtrx[x][y]),
+                  end=" " if y < len(mtrx[x]) - 1 else "")
         print()
