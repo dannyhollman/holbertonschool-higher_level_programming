3f2569e392e20565ef66a8877bd068071d418dfb
Danny Hollman <dannyhollman@gmail.com>
2019-11-26 02:55:50 +0000

    [INIT] Add all files

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates database named hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- creates table called first_table
+-- id = INT
+-- name = VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+
