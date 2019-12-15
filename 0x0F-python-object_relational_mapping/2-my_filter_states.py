d7511d33b216550833219014f3942cdbb61fae65
Danny Hollman <dannyhollman@gmail.com>
2019-12-15 03:34:15 +0000

    [UPDATE] Use .format for string

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ def main():
     db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT id, name FROM states WHERE states.name\
-            LIKE '%s' ORDER BY id ASC;" % (argv[4]))
+            LIKE '{}' ORDER BY id ASC;".format(argv[4]))
     for row in cursor.fetchall():
         print(row)
 
