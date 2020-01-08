232f2d6573c09048fc3df0a7b7ca07f3e5b985d1
Danny Hollman <dannyhollman@gmail.com>
2020-01-08 00:09:32 +0000

    [INIT] Add file 1,2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # send request to url and display body size of response
 
-curl -s "$1" | wc -c
+curl -s "$@" | wc -c
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# sends a DELETE request to URL
+
+curl -sX "DELETE" "$1"
