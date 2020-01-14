c13aa36d88124f45e0f841b4c4278b28361d164c
Danny Hollman <dannyhollman@gmail.com>
2020-01-14 03:38:35 +0000

    [UPDATE] User argv instead of string

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ displays X-Request-Id from URL """
 from urllib import request
-import sys
+from sys import argv
 
 
 def main():
     """ displays X-Request-Id from URL """
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen(argv[1]) as response:
         print(response.headers.get('X-Request-Id'))
 
 if __name__ == "__main__":
