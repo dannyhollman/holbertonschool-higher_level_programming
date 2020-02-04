a4cd7bb8355efbb0aed6ca4da81a526696ca9bf2
Danny Hollman <dannyhollman@gmail.com>
2020-02-04 17:31:24 +0000

    [UPDATE] Fix typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv[3]) { console.log('Arguments found'); } else if (process.argv[2]) { console.log('Argument found'); } else if (!process.argv[2]) { console.log('No arguments'); }
+if (process.argv[3])
+	console.log('Arguments found');
+else if (process.argv[2])
+	console.log('Argument found');
+else if (!process.argv[2])
+	console.log('No argument');
