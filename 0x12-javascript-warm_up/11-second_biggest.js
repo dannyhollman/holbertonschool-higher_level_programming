a7d971a9119146a4d6e9aebdc63561e6cb0e388f
Danny Hollman <dannyhollman@gmail.com>
2020-02-04 21:30:37 +0000

    [INIT] Add file 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,5 +7,6 @@ if (!process.argv[2] || !process.argv[3]) {
   for (let i = 2; i < process.argv.length; i++) {
     array.push(Number(process.argv[i]));
   }
-  console.log(Math.max.apply(Math, array));
+  array.sort();
+  console.log(array[array.length - 2]);
 }
