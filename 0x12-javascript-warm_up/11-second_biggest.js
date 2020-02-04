bccb161651d3039e4e8e3011ca354a8284fa7eb9
Danny Hollman <dannyhollman@gmail.com>
2020-02-04 20:20:36 +0000

    [INIT] Add file 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+if (!process.argv[2] || !process.argv[3]) {
+  console.log('0');
+} else {
+  const array = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    array.push(Number(process.argv[i]));
+  }
+  console.log(Math.max.apply(Math, array));
+}
