0d84044d2cefa9cd8ffaca77981b7654f5655f92
Danny Hollman <dannyhollman@gmail.com>
2020-02-10 19:19:43 +0000

    [UPDATE] Fix class inheritance

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle.js');
+const oldSquare = require('./5-square.js');
 
-class Square extends Rectangle {
+class Square extends oldSquare {
   constructor (size) {
     super(size, size);
   }
